6ce15977bedb567f0d2d5badc9aa5e2641c70972
Serhii Shvets <sergius.shvets@gmail.com>
2025-04-23 03:49:59 +0300

    27_Remove_Element

---
+++ Easy/27_Remove_Element/27_Remove_Element/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace _27_Remove_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var array = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+            var result = solution.RemoveElement(array, 2);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int leftPointer = 0;
+
+            for (int rightPointer = 0; rightPointer < nums.Length; rightPointer++)
+            {
+                if (nums[rightPointer] != val)
+                {
+                    nums[leftPointer] = nums[rightPointer];
+                    leftPointer++;
+                }
+            }
+
+            return leftPointer;
+        }
+    }
+}
